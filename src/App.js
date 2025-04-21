ebd06787fa4c416752dbfa1fd3f5c0d89d61f658
mohitbhargav123 <mohitbhargav688@gmail.com>
2025-04-21 11:01:52 +0530

    footer fix

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard/>}/>
+    </Routes>
+  );
+}
+
+export default App;
